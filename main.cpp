3a508759cc973ef41d75528a5ef2e0ee7120245f
Leonard Hilbert <lhilbert@communicode.de>
2019-10-15 12:39:13 +0200

    renamed compiler-namespace to java_leo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+#include <google/protobuf/compiler/plugin.h>
+#include <google/protobuf/compiler/java_leo/java_generator.h>
 
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char *argv[]) {
+    google::protobuf::compiler::java_leo::JavaGenerator generator;
+    return google::protobuf::compiler::PluginMain(argc, argv, &generator);
 }
